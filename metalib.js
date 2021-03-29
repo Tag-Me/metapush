555ae0feb574e4d8d942be78bc03f15d8e35c00c
Chi <cychoi@zoho.com>
2021-03-29 18:44:28 -0400

    Copy library over from metalib repo

---
+++ metalib.js
@@ -0,0 +1,21 @@
+if (document.location.href === 'https://www.taggingguru.com/my-team') {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function() {
+      document.title = 'Tagging Guru | Contributors | Tagging Guru Team Unite';
+      console.log('Updated title');
+    })
+    .catch(function(err) {
+      console.error('errror updating title', err);
+    });
+}
+
+if (document.location.href === 'https://www.taggingguru.com/') {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function() {
+      document.title = 'Tagging Guru | Homepage | Meta Push Update';
+      console.log('Updated title');
+    })
+    .catch(function(err) {
+      console.error('errror updating title', err);
+    });
+}
