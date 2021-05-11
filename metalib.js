f783c9e36d08ea26d0dafce60b0da67a41f6a305
Chi <cychoi@zoho.com>
2021-05-11 17:09:47 -0400

    Try unbundling JS with simple promises

--- metalib.js
+++ metalib.js
@@ -1,4 +1,31 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Fa7U":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=r;var e=function(e,t,n,o){return new(n||(n=Promise))(function(r,a){function i(e){try{u(o.next(e))}catch(t){a(t)}}function c(e){try{u(o.throw(e))}catch(t){a(t)}}function u(e){var t;e.done?r(e.value):(t=e.value,t instanceof n?t:new n(function(e){e(t)})).then(i,c)}u((o=o.apply(e,t||[])).next())})},t=function(e,t){var n,o,r,a,i={label:0,sent:function(){if(1&r[0])throw r[1];return r[1]},trys:[],ops:[]};return a={next:c(0),throw:c(1),return:c(2)},"function"==typeof Symbol&&(a[Symbol.iterator]=function(){return this}),a;function c(a){return function(c){return function(a){if(n)throw new TypeError("Generator is already executing.");for(;i;)try{if(n=1,o&&(r=2&a[0]?o.return:a[0]?o.throw||((r=o.return)&&r.call(o),0):o.next)&&!(r=r.call(o,a[1])).done)return r;switch(o=0,r&&(a=[2&a[0],r.value]),a[0]){case 0:case 1:r=a;break;case 4:return i.label++,{value:a[1],done:!1};case 5:i.label++,o=a[1],a=[0];continue;case 7:a=i.ops.pop(),i.trys.pop();continue;default:if(!(r=(r=i.trys).length>0&&r[r.length-1])&&(6===a[0]||2===a[0])){i=0;continue}if(3===a[0]&&(!r||a[1]>r[0]&&a[1]<r[3])){i.label=a[1];break}if(6===a[0]&&i.label<r[1]){i.label=r[1],r=a;break}if(r&&i.label<r[2]){i.label=r[2],i.ops.push(a);break}r[2]&&i.ops.pop(),i.trys.pop();continue}a=t.call(e,i)}catch(c){a=[6,c],o=0}finally{n=r=0}if(5&a[0])throw a[1];return{value:a[0]?a[1]:void 0,done:!0}}([a,c])}}};var n="https://kcyuajymdxnbqcoblgvg.supabase.co",o="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTU1Nzg4OSwiZXhwIjoxOTM1MTMzODg5fQ.Ft2TTnR6tTrtlqLJrh56bluS4C4JFtFHuK4d-oWn1c8";function r(){return e(this,void 0,void 0,function(){var e,r,a,i,c;return t(this,function(t){switch(t.label){case 0:return e=window.location.hostname,r=encodeURIComponent(window.location.pathname),[4,fetch(n+"/rest/v1/seo_data?select=meta_title&hostname=eq."+e+"&route=eq."+r,{headers:{Accept:"application/json",apikey:o,authorization:"Bearer "+o},method:"GET",mode:"cors"})];case 1:return[4,(a=t.sent()).json()];case 2:return i=t.sent(),a.ok?(c=i&&i.length&&i[0])&&c.meta_title&&(document.title=c.meta_title):console.error(i),[2]}})})}!function(){e(this,void 0,void 0,function(){return t(this,function(e){switch(e.label){case 0:return[4,r()];case 1:return e.sent(),[2]}})})}();
-},{}]},{},["Fa7U"], null)
-//# sourceMappingURL=/metalib.js.map
+const host = window.location.hostname;
+const route = encodeURIComponent(window.location.pathname);
+
+const supabaseUrl = 'https://kcyuajymdxnbqcoblgvg.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTU1Nzg4OSwiZXhwIjoxOTM1MTMzODg5fQ.Ft2TTnR6tTrtlqLJrh56bluS4C4JFtFHuK4d-oWn1c8';
+
+fetch(`${supabaseUrl}/rest/v1/seo_data?select=meta_title&hostname=eq.${host}&route=eq.${route}`, {
+  headers: {
+      'Accept': 'application/json',
+      'apikey': supabaseKey,
+      'authorization': `Bearer ${supabaseKey}`
+  },
+  method: 'GET',
+  mode: 'cors'
+})
+.then(function(fetchResult) {
+  return fetchResult.json();
+})
+.then(function(results) {
+  if (fetchResult.ok) {
+    const metaResult = results && results.length && results[0];
+    if (metaResult && metaResult.meta_title) {
+        document.title = metaResult.meta_title;
+    }
+  } else {
+      console.error(results);
+  }
+})
+.catch(function(err) {
+  console.error('Error updating title', err);
+});
